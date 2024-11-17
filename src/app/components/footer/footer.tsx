0649f38f7c7b8d39e228de2fa228a9ef140ce0c8
Anderson-reis7 <Anderson.200810@gmail.com>
2024-11-17 10:09:05 -0300

    adjusting names in svg files

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,13 +1,14 @@
 import { Logo } from "../logo/logo";
 
-
 export function Footer() {
   return (
-    <div className="py-8 pl-6 border-t border-2 border-neutral-900 mx-auto">
+    <div className="py-8 pl-6 border-t border-neutral-900 mx-auto">
       <div className="mb-5">
-        <Logo size={144}/>
+        <Logo size={144} />
       </div>
-      <div className="text-xs text-neutral-500">Copyright © 2024. Todos os direitos reservados</div>
+      <div className="text-xs text-neutral-500">
+        Copyright © 2024. Todos os direitos reservados
+      </div>
     </div>
   );
 }
